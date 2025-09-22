f2a6107851f4edba901fae8862e01b49fe50aea3
siendesign <victoryessien01@gmail.com>
2025-09-22 15:57:55 +0100

    bug fix test

--- app/api/currencies/route.ts
+++ app/api/currencies/route.ts
@@ -4,17 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   //mongodb connection
-  try {
-    
     db.connect();
-
     //query to mongodb
     const currencies = await Currency.find({})
-      
-  
     return NextResponse.json(currencies, { status: 200 });
-  } catch (error) {
-    console.error('Currency fetch error:', error);
-    return NextResponse.json("error: " + error, { status: 200 });
-  }
+ 
 }
